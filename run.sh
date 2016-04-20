2a2dd657e029aed47d2180eaca163fae17be4352
littlekey <littlekeyrain@gmail.com>
2016-04-20 13:56:47 -0400

    update run script

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@ CURRENT=`pwd`
 FILENAME=$1
 
 sudo cp $CURRENT/nginx.conf /etc/nginx/sites-available/$1
-sudo ln -s /etc/nginx/sites-enabled/$1
+sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1
+sudo service nginx stop
 sudo service nginx start
+python -m SimpleHTTPServer 8000 &
