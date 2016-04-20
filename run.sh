07f064241af8517c134b3979c89f5cbb45ce8e7b
littlekey <littlekeyrain@gmail.com>
2016-04-20 15:43:38 -0400

    update nginx config and run script

--- run.sh
+++ run.sh
@@ -2,8 +2,12 @@
 
 CURRENT=`pwd`
 FILENAME=$1
+TARGET_DIR=/etc/nginx/sites-available
+TARGET_LINK_DIR=/etc/nginx/sites-enabled
 
-sudo cp $CURRENT/nginx.conf /etc/nginx/sites-available/default
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+sudo cp "$CURRENT/$FILENAME" "$TARGET_DIR/$FILENAME"
+if [ -f "$TARGET_DIR/$FILENAME" ]; then
+    sudo ln -sf "$TARGET_DIR/$FILENAME" "$TARGET_LINK_DIR/$FILENAME"
+fi
 sudo service nginx stop
 sudo service nginx start
