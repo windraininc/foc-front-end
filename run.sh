9ea3724c38a5587c1ae893b756f9725558ac238a
littlekey <littlekeyrain@gmail.com>
2016-04-20 13:52:05 -0400

    update run script

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 CURRENT=`pwd`
+FILENAME=$1
 
-sudo cp $CURRENT/nginx.conf /etc/nginx/conf.d/nginx.conf
+sudo cp $CURRENT/nginx.conf /etc/nginx/sites-available/$1
+sudo ln -s /etc/nginx/sites-enabled/$1
 sudo service nginx start
