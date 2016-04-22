1d5aba562fb319c196acc35d7a2c94cd54f3d943
littlekey <littlekeyrain@gmail.com>
2016-04-22 11:06:04 -0400

    update run script

--- run.sh
+++ run.sh
@@ -2,12 +2,16 @@
 
 CURRENT=`pwd`
 FILENAME=$1
-TARGET_DIR=/etc/nginx/sites-available
-TARGET_LINK_DIR=/etc/nginx/sites-enabled
+NGINX_CONF_DIR=/etc/nginx
+TARGET_DIR=$NGINX_CONF_DIR/sites-available
+TARGET_LINK_DIR=$NGINX_CONF_DIR/sites-enabled
 
 sudo cp "$CURRENT/$FILENAME" "$TARGET_DIR/$FILENAME"
 if [ -f "$TARGET_DIR/$FILENAME" ]; then
     sudo ln -sf "$TARGET_DIR/$FILENAME" "$TARGET_LINK_DIR/$FILENAME"
 fi
-sudo service nginx stop
-sudo service nginx start
+if [ -a /var/run/nginx.pid ]; then
+    service nginx restart
+else
+    serive nginx start
+fi
