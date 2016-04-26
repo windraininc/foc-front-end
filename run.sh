a9bf7d4c5d609670bb953061cee048d2a1ee244f
LittleKey <kunyu.liao@qianmo-inc.com>
2016-04-26 17:31:12 +0800

    update run script

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ NGINX_CONF_DIR=/etc/nginx
 TARGET_DIR=$NGINX_CONF_DIR/sites-available
 TARGET_LINK_DIR=$NGINX_CONF_DIR/sites-enabled
 
+if [ ! -f "$CURRENT/$FILENAME" ]; then
+    echo "[WARNING] passing nginx configure is not a file."
+    exit -1
+fi
 sudo cp "$CURRENT/$FILENAME" "$TARGET_DIR/$FILENAME"
 if [ -f "$TARGET_DIR/$FILENAME" ]; then
     sudo ln -sf "$TARGET_DIR/$FILENAME" "$TARGET_LINK_DIR/$FILENAME"
--- run_mac.sh
+++ run_mac.sh
@@ -5,8 +5,12 @@ FILENAME=$1
 NGINX_CONF_DIR=/usr/local/etc/nginx
 TARGET_DIR=$NGINX_CONF_DIR/servers
 
-sudo cp "$CURRENT/$FILENAME" "$TARGET_DIR/$FILENAME"
-if [ -a /usr/local/var/run/nginx.pid ]; then
-    sudo nginx -s stop
+if [ ! -f "$CURRENT/$FILENAME" ]; then
+    echo "[WARNING] passing nginx configure is not a file."
+    exit -1
 fi
-sudo nginx
+sudo cp "$CURRENT/$FILENAME" "$TARGET_DIR/$FILENAME"
+if [ ! -a /usr/local/var/run/nginx.pid ]; then
+    sudo nginx
+fi
+sudo nginx -s reload
